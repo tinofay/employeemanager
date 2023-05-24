bd8d8e2a6219b2b4db22010b815c94bd5440c9e1
Tino Zungunde <tfzungunde@outlook.com>
2023-05-24 18:12:17 +0200

    update ContractorRepositoryTest

--- src/test/java/co/zw/company/employeemanager/ContractorRepositoryTest.java
+++ src/test/java/co/zw/company/employeemanager/ContractorRepositoryTest.java
@@ -3,6 +3,7 @@ package co.zw.company.employeemanager;
 import co.zw.company.employeemanager.entity.Contractor;
 import co.zw.company.employeemanager.repository.ContractorRepository;
 import org.assertj.core.api.Assertions;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +12,7 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.time.LocalDate;
+
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
@@ -21,23 +23,37 @@ public class ContractorRepositoryTest {
     @Autowired
     private TestEntityManager testEntityManager;
 
-    @Test
-    public void findContractorByStartDate_shouldReturnContractor(){
-        //given
-        Contractor contractor = new Contractor();
+    private Contractor contractor;
+
+    @Before
+    public void setUp(){
+        contractor = new Contractor();
         contractor.setName("Tino Zee");
         contractor.setEmail("tinozee@contractor.com");
         contractor.setContractStartDate(LocalDate.of(2023, 1, 1));
         contractor.setContractEndDate(LocalDate.of(2023, 6, 30));
-        contractorRepository.save(contractor);
+    }
 
-        //when
-        Contractor foundContractor = contractorRepository.findContractorByContractStartDate(LocalDate.of(2023, 1, 1));
 
-        //then
-        Assertions.assertThat(foundContractor).isNotNull();
-        Assertions.assertThat(foundContractor.getName()).isEqualTo("Tino Zee");}
+    @Test
+    public void findContractorByStartDate_shouldReturnContractor(){
 
+        testEntityManager.persist(contractor);
+        testEntityManager.flush();
+
+        contractorRepository.findContractorByContractStartDate(LocalDate.of(2023, 1, 1));
+
+        Assertions.assertThat(contractor).isNotNull();
+    }
+
+    @Test
+    public void saveContractor_shouldReturnContractor(){
+
+        testEntityManager.persist(contractor);
+        testEntityManager.flush();
+
+        Assertions.assertThat(contractor.getName()).isEqualTo("Tino Zee");
+    }
 
 
 }
