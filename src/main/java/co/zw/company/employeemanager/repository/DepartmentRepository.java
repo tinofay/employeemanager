1b6279e8ca9472ac2eb0d74ea2434c397042b2e0
dickonkudzai <dickonkudzai@live.com>
2023-06-18 11:24:28 +0200

    version on department

--- src/main/java/co/zw/company/employeemanager/repository/DepartmentRepository.java
+++ src/main/java/co/zw/company/employeemanager/repository/DepartmentRepository.java
@@ -2,19 +2,20 @@ package co.zw.company.employeemanager.repository;
 
 import co.zw.company.employeemanager.entity.Department;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.stereotype.Repository;
 
+import javax.persistence.LockModeType;
 import java.util.List;
 
 @Repository
 public interface DepartmentRepository extends JpaRepository<Department, Long> {
-    // create
     List<Department> findAll();
 
     Department findById(long id);
 
     Department findByName(String name);
-
+    @Lock(LockModeType.OPTIMISTIC_FORCE_INCREMENT)
     Department save(Department department);
 
     void deleteById(long id);
